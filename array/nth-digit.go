3ad1357d39c0b275e53d798e27d952fddb6a3869
xiaoyuer <1961383326@qq.com>
2020-07-05 16:11:33 +0800

    optimised nth

--- array/nth-digit.go
+++ array/nth-digit.go
@@ -19,3 +19,18 @@ func findNthDigit(n int) int {
     res, _ := strconv.Atoi(string(str[idx]))
     return res
 }
+
+
+//optimised
+func findNthDigit(n int) int {
+    n -= 1
+    for digit := 1; digit < 11; digit++ {
+        firstNum := math.Pow(10, float64(digit-1))
+        if n - 9*int(firstNum)*digit < 0 {
+            res, _ := strconv.Atoi(string(strconv.Itoa(int(firstNum)+n/digit)[n%digit]))
+            return res
+        }
+        n -= 9*int(firstNum)*digit
+    }
+    return 0
+}
