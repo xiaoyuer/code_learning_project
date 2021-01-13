b31a10b30cf2353cadcc42ba8702715e00b7def9
jiyu <ji.yu@aminer.cn>
2021-01-13 18:07:42 +0800

    binary method

--- binary_method/sqrtx.go
+++ binary_method/sqrtx.go
@@ -1,7 +1,21 @@
 func mySqrt(x int) int {
-	res := x
-	for res*res > x {
-		res = (res + x/res) / 2
+	l, r := 0, x
+	for l < r {
+		mid := (l + r + 1) >> 1
+		if mid <= x/mid {
+			l = mid
+		} else {
+			r = mid - 1
+		}
+
 	}
-	return res
+	return l
 }
+
+// func mySqrt(x int) int {
+// 	res := x
+// 	for res*res > x {
+// 		res = (res + x/res) / 2
+// 	}
+// 	return res
+// }
