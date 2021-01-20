7b50f7c15aef5265944a58dc38785f15da6e38e9
jiyu <ji.yu@aminer.cn>
2021-01-20 12:05:44 +0800

    sh

---
+++ shell/transpose-file.sh
@@ -0,0 +1,17 @@
+awk '{
+    for (i=1;i<=NF;i++){
+        if (NR==1){
+            res[i]=$i
+        }
+        else{
+            res[i]=res[i]" "$i
+        }
+    }
+}END{
+    for(j=1;j<=NF;j++){
+        print res[j]
+    }
+}' file.txt
+
+
+#NF是当前行的field字段数；NR是正在处理的当前行数。
